aa86aaeabb6209bd1d38d0fcdad96fdd4f015a6d
Arpan Gupta <arpan.gupta@lpu.in>
2024-01-22 10:05:17 +0530

    new pg

---
+++ Nodejs/scope.js
@@ -0,0 +1,19 @@
+a(30);
+function a(b)
+{
+    console.log("inside the function",b);
+}
+let x;
+const y=9;
+var z=70;
+{
+let x=9;
+console.log("i'm inside the block my value is :",x)
+}
+{
+const y=10;
+console.log("i'm inside the block my value is :",y)
+}
+var z=90;
+z="arpan is gay"
+console.log("i'm not inside the block and my value is:",z);

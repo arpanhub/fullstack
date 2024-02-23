89562bbdf75adde46e13c36b8449e46baf484ad7
Arpan Gupta <arpan.gupta@lpu.in>
2024-02-23 13:15:01 +0530

    inital commit

--- Nodejs/scope.js
+++ Nodejs/scope.js
@@ -1,4 +1,4 @@
-/* a(30);
+/* /* a(30);
 function a(b)
 {
     console.log("inside the function",b);
@@ -17,10 +17,15 @@ console.log("i'm inside the block my value is :",y)
 var z=90;
 z="arpan is gay"
 console.log("i'm not inside the block and my value is:",z); */
-var x = 24;
+/* var x = 24;
 {
   y = 11;
   x = !isNaN(y);
   z = x + y;
   console.log(z);
-}
+} */
+const buf1 = Buffer.from('This is Node.js class');
+const buf2 = Buffer.from('67876543276','hex');
+/* console.log(buf1.tostring('hex',5,15));
+console.log(buf2.tostring('hex',5,15)); */
+console.log("hello wolrd");

6493c9750236f3b28314500e29ec3eb67315017a
Arpan Gupta <arpan.gupta@lpu.in>
2024-02-28 11:45:35 +0530

    Added changes Please enter the commit message for your changes. Lines starting

--- Nodejs/buffer.js
+++ Nodejs/buffer.js
@@ -1,5 +1,45 @@
-const buf1 = Buffer.from('This is Node.js class');
-const buf2 = Buffer.from('67876543276','hex');
+// const buf1 = Buffer.from('This is Node.js class');
+// const buf2 = Buffer.from('67876543276','hex');
 /* console.log(buf1.tostring('hex',5,15));
 console.log(buf2.tostring('hex',5,15)); */
-console.log("hello wolrd");
+/* console.log("hello wolrd");
+const buf1=Buffer.from('this is arpan');
+const buf2=Buffer.from('67876543276','hex');
+console.log(buf2.toString('hex',5,15)); */
+
+/* const buf3=Buffer.from('Abcdefgah');
+console.log(buf3.includes(61));
+
+const buf= Buffer.alloc(5);
+console.log(buf.fill('hello',3,4)); */
+
+/* const buf1 = Buffer.from('This is Node.js class');
+const buf2 = Buffer.from('54686973206973204e6f64652e6a7320636c617373','hex');
+console.log(buf1.toString('hex',5,15));
+console.log(buf2.toString('utf8',5,10)); */
+/* 
+buf=new Buffer.alloc(50);
+n=buf.write('This is Nodejs class ');
+console.log('The number of octets are:'+n); */
+
+/* var buf1 = Buffer.from('wow');
+var buf2= Buffer.from('hello');
+var buf3 = Buffer.concat([buf1,buf2],5);
+console.log("The contents are " + buf3);
+ */
+/* var buffer1 = Buffer.from('ABCD');
+var buffer2 = Buffer.from('ABcD');
+var result = buffer1.compare(buffer2);
+if(result === 0)
+{
+console.log(buffer1+"is equal to "+ buffer2);
+}
+else
+{
+console.log(buffer1+"is not equal to"+ buffer2);
+}
+ */
+var buffer1 = Buffer.from('This is Nodejs');
+var buffer2 = Buffer.alloc(10);
+buffer1.copy(buffer2,4,4,10);
+console.log("buffer2 content: " + buffer2.toString());
--- Nodejs/p0.js
+++ Nodejs/p0.js
@@ -1,13 +1,25 @@
-const http = require('http');
+/* const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World");
+    res.end("hello guys welcome back to my youtube channel");
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
+}); */
+
+/* const http= require('http');
+const hostname='127.0.0.1';
+const port = 3000;
+const server=http.createServer((req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-type','text/plain');
+    res.end("yooooooooooo");
 });
+server.listen(port,hostname,()=>{
+    console.log(`server running ar http://${hostname}:${port}/`);
+}); */

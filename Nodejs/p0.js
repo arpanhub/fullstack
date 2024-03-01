17d08a34e4f2244ca4d708576a02ce0ad26b8ca3
Arpan Gupta <arpan.gupta@lpu.in>
2024-03-01 13:35:09 +0530

    added Jquery

--- Nodejs/p0.js
+++ Nodejs/p0.js
@@ -23,3 +23,25 @@ const server=http.createServer((req,res)=>{
 server.listen(port,hostname,()=>{
     console.log(`server running ar http://${hostname}:${port}/`);
 }); */
+/* const sym1=Symbol('foo');
+const sym2=Symbol('bar');
+const obj ={
+    [sym1]:'value',
+    [sym2]:'value2',
+
+};
+console.log(obj.sym2); */
+/* let y=10;
+do{
+    console.log("Hi");
+    y-=Math.pow(2,y%3);
+} while(y>0);
+ */
+/* const arr = Array.from({ length: 5 }, (_, i) => i * i);
+console.log(arr); // Output: [0, 1, 4, 9, 16]
+ */
+const buffer1=Buffer.alloc(10);
+const buffer2=Buffer.from([1,2,3,4,5]);
+const buffer3=Buffer.from('hello','utf-8');
+const buffer4=Buffer.alloc(20);
+console.log(buffer1.length+buffer2.length+buffer3.length+buffer4.length);

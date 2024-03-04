4a4bbebb75236a1c994aff1329d772f057e1864c
arpanhub <arpan.gupta@lpu.in>
2024-03-04 04:31:08 +0000

    Your commit message

--- Nodejs/CA1.js
+++ Nodejs/CA1.js
@@ -7,7 +7,7 @@ console.log(x);
 const y=x+1n;
 console.log(y);k
    */
-var fs=require("fs");
+/* var fs=require("fs");
 var data=fs.readFileSync("file.txt");
 if(data=='')
 {
@@ -15,4 +15,23 @@ if(data=='')
 }
 else{
   console.log("Synchronous read :"+data.toString());
-}
+} */
+
+// function sum(n)
+// {
+//   i=0;
+//   while(n<15)
+//     {
+//       i +=n;
+//       n--;
+//     }
+//   console.log("The sum is:"+1);
+// }
+// sum(10);
+ var c = setInterval(function() {
+  console.log("GUPTA");
+}, 3000);
+
+setTimeout(function() {
+  clearInterval(c);
+}, 10000);  

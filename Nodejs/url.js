e01933f10af399fa3a12cea4190778bbadcd9eca
Arpan Gupta <arpan.gupta@lpu.in>
2024-03-18 10:39:46 +0530

    express modules

--- Nodejs/url.js
+++ Nodejs/url.js
@@ -11,7 +11,7 @@ http.createServer(function (req, res) {
       return res.end("404 Page Not Found");}  
     res.writeHead(200, {'Content-Type': 'text/html'}); 
     res.write(data); 
-    return res.end(); 
+    return res.end();
   }); 
 }).listen(8080); 
 console.log("Server is Running at \\localhost:8080")
